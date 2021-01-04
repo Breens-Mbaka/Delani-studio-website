bff403852e0b16ecd4fec6afc26fed7d2f9339bd
Breens-Thomas <mbakabreens@gmail.com>
2021-01-04 17:34:03 +0300

    Add toggle of description and illustration when clicked

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $("#image1").click(function(){
+        $("#description-service1").show()
+        $("#image1").hide()
+    });
+    $("#description-service1").click(function(){
+        $("#image1").show()
+        $("#description-service1").hide()
+    })
+    $("#image2").click(function(){
+        $("#description-service2").show()
+        $("#image2").hide()
+    });
+    $("#description-service2").click(function(){
+        $("#image2").show()
+        $("#description-service2").hide()
+    })
+    $("#image3").click(function(){
+        $("#description-service3").show()
+        $("#image3").hide()
+    });
+    $("#description-service3").click(function(){
+        $("#image3").show()
+        $("#description-service3").hide()
+    })
+})
